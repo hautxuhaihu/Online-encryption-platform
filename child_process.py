e4fec54810873934f092aef5801f463888bd346f
hautxuhaihu <1829755168@qq.com>
2019-06-06 09:29:51 +0800

    成功将架构改为多进程方式，http_server_multithreading中有一个事件循环，监视者http请求和挂着子进程。子进程用与计算。

--- child_process.py
+++ child_process.py
@@ -6,13 +6,41 @@
 # @Software: PyCharm
 
 from twisted.internet import reactor
-import sys, os
-sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 1)
+from aes import MyAES
+import sys, json
+from twisted.internet.threads import deferToThreadPool
+from twisted.python.threadpool import ThreadPool
+
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
+sys.stdin = open(sys.stdin.fileno(), mode='r', encoding='utf-8', buffering=1)
+
+# 初始化并启动线程池
+myThreadPool = ThreadPool(1, 5, 'myThreadPool')
+myThreadPool.start()
+
 
 def main():
-    input = sys.stdin.readline().strip('\n')
-    print(input)
+    request_str = sys.stdin.readline().strip()
+
+    # json解析数据
+    request_json = json.loads(request_str)
+    # 判断用户发送的数据中是否包括关键元素
+    if "to_en_data" in request_json and "password" in request_json:
+        # 调用模块加密
+        # myAES = MyAES(request_json["password"])
+        # encrypt_result = myAES.aes_encrypt(request_json["to_en_data"])
+        # print(encrypt_result)
+        deferToThreadPool(reactor, myThreadPool, encrypt, request_json)
+    else:
+        print("not found to_en_data or password")
+
     reactor.callLater(0, main)
 
+def encrypt(request_json):
+
+    myAES = MyAES(request_json["password"])
+    encrypt_result = myAES.aes_encrypt(request_json["to_en_data"])
+    print(encrypt_result)
 
 reactor.run(main())
+
--- http_server_multithreading.py
+++ http_server_multithreading.py
@@ -5,15 +5,16 @@
 # @File    : http_server_multithreading.py
 # @Software: PyCharm
 
-
 from twisted.logger import jsonFileLogObserver, Logger
 from twisted.web import server, resource
 from twisted.internet import reactor, endpoints, protocol
-from twisted.internet.threads import deferToThreadPool
-from twisted.python.threadpool import ThreadPool
 from aes import MyAES
 import json, os
 from read_ini import ReadINI
+
+
+log = Logger(observer=jsonFileLogObserver(open("myLog/log.json", "a")),
+                     namespace="http_server_multithreading")
 
 
 class MyPP(protocol.ProcessProtocol):
@@ -22,13 +23,16 @@ class MyPP(protocol.ProcessProtocol):
 
     def connectionMade(self):
         print("connectionMade!")
+        # print("xuhaihu\n".encode())
         # self.transport.write("xuhaihu\n".encode())
         # self.transport.closeStdin()
 
     def outReceived(self, data):
         print("outReceived! with %d bytes!" % len(data))
-        self.data = self.data + data.decode()
+        self.data = data.decode()
         print(self.data)
+        requestHandler.request.write(data)
+        requestHandler.request.finish()
 
     def errReceived(self, data):
         print("errReceived! with %d bytes!" % len(data))
@@ -53,14 +57,8 @@ class MyPP(protocol.ProcessProtocol):
 
 
 myPP = MyPP()
-
-
-# 初始化并启动线程池
-myThreadPool = ThreadPool(1, 5, 'myThreadPool')
-myThreadPool.start()
-# 实例化Logger
-log = Logger(observer=jsonFileLogObserver(open("myLog/log.json", "a")),
-             namespace="http_server_multithreading")
+reactor.spawnProcess(myPP, os.environ['VIRTUAL_ENV']+"/bin/python", [os.environ['VIRTUAL_ENV']+"/bin/python", "child_process.py"],
+                     childFDs={0:"w", 1:"r", 2:2, 3:"w", 4:"r"})
 
 
 def encrypt_task(request):
@@ -77,6 +75,7 @@ def encrypt_task(request):
         # 判断用户发送的数据中是否包括关键元素
         if "to_en_data" in request_json and "password" in request_json:
             # 调用模块加密
+            myPP.transport.write((request_json["password"]+"\n").encode())
             myAES = MyAES(request_json["password"])
             encrypt_result = myAES.aes_encrypt(request_json["to_en_data"])
             request.write(encrypt_result.encode())
@@ -102,18 +101,21 @@ class RequestHandler(resource.Resource):
         :param request:客户端发送过来的请求
         :return: 返回客户端AJAX状态值
         """
-        deferToThreadPool(reactor, myThreadPool, encrypt_task, request)
+        self.request = request
+        request_str = request.content.read()+"\n".encode()
+        myPP.transport.write(request_str)
+        # myPP.transport.closeStdin()
+        # deferToThreadPool(reactor, myThreadPool, encrypt_task, request)
         log.info("Got data: {data}.", data="AJAX status value is 1")
         return 1
 
 
 # 监听8080端口，并开始twisted的事件循环
-site = server.Site(RequestHandler())
+requestHandler = RequestHandler()
+site = server.Site(requestHandler)
 port = int(ReadINI("config.ini").read("config", "port"))
 endpoint = endpoints.TCP4ServerEndpoint(reactor, port)
 endpoint.listen(site)
 log.info("{info}", info="Http server is listening port %s" % port)
-reactor.spawnProcess(myPP, os.environ['VIRTUAL_ENV']+"/bin/python", [os.environ['VIRTUAL_ENV']+"/bin/python", "child_process.py"],
-                     childFDs={0:"w", 1:"r", 2:2, 3:"w", 4:"r"})
 reactor.run()
 
